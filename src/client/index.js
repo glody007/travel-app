cb2659af931b08af43d35fefb87fa477dfa5644f
glody007 <glodymbutwile@gmail.com>
2022-07-11 12:33:31 +0200

    feat: header, form and footer desings

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,8 @@
 import { handleSubmit } from "./js/formHandler";
 
+import './styles/reset.scss'
+import './styles/header.scss'
+import './styles/home.scss'
+import './styles/footer.scss'
+
 export { handleSubmit }
