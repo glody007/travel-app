e8f03ea74d775a479d23d458bef80450236fb0b5
glody007 <glodymbutwile@gmail.com>
2022-07-13 16:40:49 +0200

    fix: Set port 9000

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,8 @@
 const app = require('./app')
 
-const port = process.env.PORT || 8000
+const port = 9000
 
 // designates what port the app will listen to for incoming requests
 app.listen(process.env.PORT, function () {
-    console.log(`Example app listening on port ${port}!`)
+    console.log(`App listening on port ${port}!`)
 })
